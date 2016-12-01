eaa68b26989351235a00db359d6b21eaa01c1b2f
sherri010 <aminshahidi.sh@gmail.com>
2016-12-01 15:09:55 -0800

    init project

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+  console.log("server hit");
+  res.sendFile(__dirname+"/views/index.html")
+});
+
+
+app.listen(port,function(){
+  console.log('Server fired on ',port);
+});
